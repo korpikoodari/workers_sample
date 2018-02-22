d89d7b70643cf994a8b03c07aee70757e88a2164
korpikoodari <pasi@korpikoodarit.eu>
2018-02-22 02:50:57 +0000

    css styling changes, chaning the mock data context

--- src/app/mock-workers.ts
+++ src/app/mock-workers.ts
@@ -1,14 +1,65 @@
 import { Employee } from './employee';
 import {Role} from './role'
 export const EMPLOYEES: Employee[] = [
-  { id: 11, name: 'Mr. Nice' ,role: Role.Developer},
-  { id: 12, name: 'Narco' ,role:  Role.Operator},
-  { id: 13, name: 'Bombasto',role : Role.Manager },
-  { id: 14, name: 'Celeritas' ,role : Role.Owner},
-  { id: 15, name: 'Magneta' ,role : Role.Owner},
-  { id: 16, name: 'RubberMan' ,role :Role.Manager},
-  { id: 17, name: 'Dynama' ,role: Role.Operator},
-  { id: 18, name: 'Dr IQ' ,role :Role.Developer},
-  { id: 19, name: 'Magma' ,role : Role.Developer},
-  { id: 20, name: 'Tornado',role :Role.Developer }
+
+   {
+  id  : 1,
+  name: 'Brooklynn Rios',
+  role: Role.Developer
+    },
+    {
+  id  : 2,
+  name: 'Maurice Cortez',
+  role: Role.Developer
+    },
+    {
+  id  : 3,
+  name: 'Eliana Novak',
+  role:Role.Manager
+    },
+    {
+  id  : 4,
+  name: 'Aliana Perry',
+  role: Role.Developer
+    },
+    {
+  id  : 5,
+  name: 'Madelyn Kramer',
+  role: Role.Manager
+    },
+    {
+  id  : 6,
+  name: 'Delilah Goodwin',
+  role: Role.Manager
+    },
+    {
+  id  : 7,
+  name: 'Matt Davidson',
+   role: Role.Developer
+    },
+    {
+  id  : 8,
+  name: 'Amina Morrison',
+  role : Role.Owner
+    },
+    {
+  id  : 9,
+  name: 'Delaney Richard',
+  role: Role.Developer
+    },
+    {
+  id  : 10,
+  name: 'Bruce Walter',
+  role:  Role.Operator
+    },
+    {
+  id  : 11,
+  name: 'Gwendolyn Castro',
+  role : Role.Owner
+    },
+    {
+  id  : 12,
+  name: 'Mekhi Wolfe',
+  role: Role.Developer
+    }
 ];
